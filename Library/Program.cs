625eff52b592bc7f06b091b2f5280edd8a4cf34e
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-02 21:02:23 -0400

    Added the Index View, Populated the Database as well!

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,11 +1,16 @@
 
 using Library;
+using LibraryData;
+using LibraryServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//If you are having problems, look into more detail of services.AddSingleton(Configuration)
+builder.Services.AddScoped<ILibraryAsset, LibraryAssetService>();
 
 builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("LibraryConnection")
