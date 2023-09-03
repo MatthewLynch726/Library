5aca8993c75478e85177bfecdb29ff2039fad482
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-02 20:21:43 -0400

    Adding Models that are used for a single page.

---
+++ Library/Controllers/CatalogController.cs
@@ -0,0 +1,33 @@
+﻿using Library.Models.Catalog;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+
+        private ILibraryAsset _assets;
+
+        public CatalogController(ILibraryAsset assets) 
+        {
+            _assets = assets;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _assets.GetType(result.Id)
+                });
+        }
+    }
+}
---
+++ Library/Models/AssetIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using Library.Models.Catalog;
+
+namespace Library.Models
+{
+    public class AssetIndexModel
+    {
+        public IEnumerable<AssetIndexListingModel> Assets { get; set; }
+    }
+}
