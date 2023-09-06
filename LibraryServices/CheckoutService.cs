eff5812e1b38685b0566085dd587a9e304a087a7
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-06 17:06:24 -0400

    Adding methods in CheckoutServices

--- LibraryServices/CheckoutService.cs
+++ LibraryServices/CheckoutService.cs
@@ -62,22 +62,143 @@ namespace LibraryServices
 
         public void CheckInItem(int assetId, int libraryCardId)
         {
-            throw new NotImplementedException();
+            var now = DateTime.Now;
+
+            var item = _context.LibraryAssets
+                .FirstOrDefault(a => a.Id == assetId);
+
+            //remove any existing checkjouts on the item
+            RemoveExistingCheckouts(assetId);
+            //close any existing checkout hisotyr. 
+            CloseExistingCheckoutHistory(assetId, now);
+            //look for exitsting holds on the item
+            var currentHolds = _context.Holds
+                .Include(h => h.LibraryAsset)
+                .Include(h => h.LibraryCard)
+                .Where(h => h.LibraryAsset.Id == assetId);
+            //if there are holds, checkout the item to the librarycard with the rearliset hold
+            if (currentHolds.Any())
+            {
+                CheckoutToEarliestHold(assetId, currentHolds);
+            }
+            //otherwise, update the item status to avaliable.
+            UpdateAssetStatus(assetId, "Available");
+
+            _context.SaveChanges();
+        }
+
+        private void CheckoutToEarliestHold(int assetId, IQueryable<Hold> currentHolds)
+        {
+            var earliestHold = currentHolds.OrderBy(holds => holds.HoldPlaced)
+                .FirstOrDefault();
+
+            var card = earliestHold.LibraryCard;
+
+            _context.Remove(earliestHold);
+            _context.SaveChanges();
+            CheckOutItem(assetId, card.Id);
         }
 
         public void CheckOutItem(int assetId, int libraryCardId)
         {
-            throw new NotImplementedException();
+            if (IsCheckedOut(assetId))
+            {
+                return;
+                // Add logic here to handle feedback to the user
+            }
+
+            var item = _context.LibraryAssets
+                .FirstOrDefault(a => a.Id == assetId);
+
+            UpdateAssetStatus(assetId, "Checked Out");
+            var libraryCard = _context.LibraryCards
+                .Include(card => card.Checkouts)
+                .FirstOrDefault(card => card.Id == libraryCardId);
+
+            var now = DateTime.Now;
+
+            var checkout = new Checkout
+            {
+                LibraryAsset = item,
+                LibraryCard = libraryCard,
+                Since = now,
+                Until = GetDefaultCheckoutTime(now)
+            };
+
+            _context.Add(checkout);
+
+            var checkoutHistory = new CheckoutHistory
+            {
+                CheckedIn = now,
+                LibraryAsset = item,
+                LibraryCard = libraryCard
+            };
+
+            _context.Add(checkoutHistory);
+            _context.SaveChanges();
         }
 
-        public void MarkFound(int assetId) //Where ended VIdeo
+        private DateTime GetDefaultCheckoutTime(DateTime now)
         {
-            throw new NotImplementedException();
+            return now.AddDays(30);
+        }
+
+        private bool IsCheckedOut(int assetId)
+        {
+            var isCheckedOut = _context.Checkouts.Where(co=>co.LibraryAsset.Id == assetId).Any();
+            return isCheckedOut;
+        }
+
+        public void MarkFound(int assetId)
+        {
+
+            var now = DateTime.Now;
+            
+
+            UpdateAssetStatus(assetId, "Available");
+
+            RemoveExistingCheckouts(assetId);
+            CloseExistingCheckoutHistory(assetId, now);
+
+            _context.SaveChanges();
+        }
+
+        private void UpdateAssetStatus(int assetId, string v)
+        {
+            var item = _context.LibraryAssets.FirstOrDefault(a => a.Id == assetId);
+
+            _context.Update(item);
+
+            item.Status = _context.Statuses.FirstOrDefault(status => status.Name == "Available");
+        }
+
+        private void CloseExistingCheckoutHistory(int assetId, DateTime now)
+        {
+            var history = _context.CheckoutsHistories.FirstOrDefault(h => h.LibraryAsset.Id == assetId && h.CheckedIn == null);
+
+            if (history != null)
+            {
+                _context.Update(history);
+                history.CheckedIn = now;
+            }
+        }
+
+        private void RemoveExistingCheckouts(int assetId)
+        {
+            //remove any existing checkouts on the item
+            var checkout = _context.Checkouts.FirstOrDefault(co => co.LibraryAsset.Id == assetId);
+
+            if (checkout != null)
+            {
+                _context.Remove(checkout);
+            }
         }
 
         public void MarkLost(int assetId)
         {
-            throw new NotImplementedException();
+
+            UpdateAssetStatus(assetId, "Lost");
+            _context.SaveChanges();
         }
 
         public void PlaceHold(int assetId, int libraryCardId)
